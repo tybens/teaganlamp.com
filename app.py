66c0a617b8e21edf035cb283a8203d02846d75c2
tybens <tb19@princeton.edu>
2020-11-26 11:57:12 -0800

    make into websocket to listen for any lamp changes

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import os
 
 from flask import Flask, jsonify, send_from_directory
 from flask_cors import CORS
+from flask_socketio import SocketIO, send, emit
 
 # check if in production and change static location
 dir_path = os.path.dirname(os.path.realpath(__file__))
@@ -13,6 +14,7 @@ static_folder = '/var/www/static' if (dir_path == '/srv/teagalamp') else 'static
 app = Flask(__name__, static_url_path='', static_folder=static_folder) 
 CORS(app)
 app.config['SECRET_KEY'] = 'secret!!'
+socketio = SocketIO(app, cors_allowed_origins='*')
 
 
 isLampOn = False
@@ -27,8 +29,10 @@ def getLamp():
 @app.route('/changeLamp')
 def changeLamp():
     global isLampOn
-
     # do something that changes lamp... idk how
     isLampOn = not isLampOn
-
+    socketio.emit('lamp changed', isLampOn)
     return {'success': 200}
+
+if __name__ == "__main__":
+    socketio.run(app)
