8cd4088bee89ee7de6285007143f48bed12342a8
tybens <tb19@princeton.edu>
2020-11-29 01:02:38 -0800

    big changes

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,12 @@
 import json
 import datetime
 import os
+import redis
 
-from flask import Flask, jsonify, send_from_directory
+from flask import Flask, request, jsonify, send_from_directory, make_response
 from flask_cors import CORS
 from flask_socketio import SocketIO, send, emit
+from flask_pymongo import PyMongo
 
 PROD = False
 # check if in production and change static location
@@ -18,7 +20,17 @@ CORS(app)
 app.config['SECRET_KEY'] = 'secret!!'
 socketio = SocketIO(app, cors_allowed_origins='*')
 
-totalClicks = 0
+player_redis = redis.Redis(decode_responses=True, db=0)
+player_redis.hset('leaderboard', {'teagan_poopoo':0})
+
+
+if player_redis.exists('totalClicks'):
+    totalClicks = player_redis.get('totalClicks')
+else:
+    player_redis.set('totalClicks', 0)
+    totalClicks = 0
+
+
 isLampOn = False
 
 @app.route('/')
@@ -39,11 +51,39 @@ def changeLamp():
     global totalClicks
 
     totalClicks += 1
-    # do something that changes lamp... idk how
     isLampOn = not isLampOn
 
+    username = request.get_json().username
+    newClicks = player_redis.get(username)+1
+    player_redis.set(newClicks)
+    player_redis.hset(player_redis.hgetall('leaderboard').update({username: newClicks}))
+
     socketio.emit('lamp changed', {'isLampOn':isLampOn, 'totalClicks':totalClicks})
-    return {'success': 200}
+    # do something that changes PHYSICAL lamp... idk how
+    return make_response({'userClicks': newClicks}, 200)
+
+@app.route('/createUser')
+def createUser():
+
+    username = request.get_json().username  
+
+    alreadyExists = player_redis.exists(username)
+
+    if alreadyExists:
+        return make_response({'Error': 'Username already exists!!'}, 400)
+    
+    players_redis.set(username, 0)
+    players_redis.hset(players_redis.hgetall('leaderboard').update({username: 0}))
+
+    # maybe emit PLAYER CREATED (to call leaderboard)
+    return make_response({'success': 'player created'}, 200)
+
+@app.route('/leaderboard')
+def leaderboard():
+    leaderboard = player_redis.hgetall('leaderboard')
+
+    return make_response(jsonify(leaderboard), 200)
+
 
 if __name__ == "__main__":
     socketio.run(app)
