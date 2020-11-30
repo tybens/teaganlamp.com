6e1701e477ad4bf65dd2bffce1900fb25db8ef77
tybens <tb19@princeton.edu>
2020-11-30 14:41:05 -0800

    add functioning personal clicks, creating user, logging in

--- static/client.js
+++ static/client.js
@@ -5,123 +5,141 @@ var socket = io.connect(server);
 const form = document.getElementById('username-form');
 const lampButton = document.getElementById('lamp-button');
 const clickCount = document.getElementById('click-count');
-const usernameInput = document.getElementById('username-input')
-const introSection = document.getElementById('intro-text')
-const bodySection = document.getElementById('content-wrap')
+const usernameInput = document.getElementById('username-input');
+const introSection = document.getElementById('intro-text');
+const bodySection = document.getElementById('content-wrap');
+const userDiv = document.getElementById('user-div');
 
 var username;
 var userClicks;
 if (localStorage.getItem('username') == null) {
-// show and get username input
+  // show and get username input
 
 
 } else {
-introSection.classList.remove('blurry-text');
-form.style.display = 'none';
-bodySection.style.display = '';
-username = localStorage.getItem('username');
-console.log('Welcome Back! ' + username)
-getLamp()
+  introSection.classList.remove('blurry-text');
+  bodySection.style.display = '';
+  form.style.display = 'none';
+  username = localStorage.getItem('username');
+  document.getElementById('username').innerText = username;
 
+  console.log('Welcome Back! ' + username);
+  getLamp() // updates lamp and total clicks and user clicks
+  userDiv.style.opacity = 1;
 }
 
 // need to fetch status of lamp (on or off) from server
 socket.on('lamp changed', function(response) {
-// hopefully this updates my client isLampOn variable
-changeLampButton(response.isLampOn, response.totalClicks)
+  // hopefully this updates my client isLampOn variable
+  changeLampButton(response.isLampOn, response.totalClicks)
 
-// TODO: fetch request for leaderboard and update
-fetch(`${server}/leaderboard`)
-  .then(response => response.json())
-  .then(leaderboard => {
+  // TODO: fetch request for leaderboard and update
+  fetch(`${server}/leaderboard`)
+    .then(response => response.json())
+    .then(leaderboard => {
 
-})
+  })
 });
 
 
-async function getLamp() {
-fetch(`${server}/getLampAndClicksAndUserClicks`, {
-  method: 'POST',
-  body: JSON.stringify({'username': username}),
-  headers: {
-    'content-type': 'application/json'
-  }
-}).then(response => response.json())
-  .then(response => {
-    changeLampButton(response.isLampOn, response.totalClicks);
-    userClicks = parseInt(response.userClicks)
-  }).catch((error) => {
-    console.error('Error:', error);
-});
-}
-
-
-async function changeLampButton(isLampOn, totalClicks) {
-if (!isLampOn) {
-  lampButton.checked = false
-
-} else {
-  lampButton.checked = true
-}
-
-clickCount.innerText = 'THIS LAMP HAS BEEN CHANGED ' + String(totalClicks) + ' TIMES'
-}
-
-async function changeLeaderboard(leaderboard) {
-// TODO: !!
-}
-
-async function doLamp() {
-// fetch request to change lamp
-userClicks += 1; // local clicks
-
-fetch(`${server}/changeLamp`, {
-  method: 'POST',
-  body: JSON.stringify({'username': username}),
-  headers: {
-    'content-type': 'application/json'
-  }
-}).then(response => response.json())
-  .then(response => {
-    // console.log(response)
-
-  }).catch((error) => {
-    console.error('Error:', error);
-  });
-} 
-
-function createUser(ele) {
-if (event.keyCode == 13) {
-  event.preventDefault();
-  console.log('ENTER')
-  username = ele.value
-
-  fetch(`${server}/createUser`, {
+function getLamp() {
+  fetch(`${server}/getLampAndClicksAndUserClicks`, {
     method: 'POST',
     body: JSON.stringify({'username': username}),
     headers: {
       'content-type': 'application/json'
     }
-  }).then(response => {
-    if (response.status === 200) {
-        localStorage.setItem('username', username);
-        userClicks = 0;
-        form.style.display = 'none';
-        bodySection.style.display = '';
-        console.log('Welcome! ' + localStorage.getItem('username'))
-        // TODO: (maybe) show leaderboard
-    } else {
-      response.json().then(error => {
-        console.log(error)
-        // TODO: show username error (username already exists)
-      })
+  }).then(response => response.json())
+    .then(response => {
+      changeLampButton(response.isLampOn, response.totalClicks);
+      userClicks = parseInt(response.userClicks);
+
+      document.getElementById('userClicks').innerText = 'clicks: ' + String(userClicks);
+    }).catch((error) => {
+      console.error('Error:', error);
+  });
+}
+
+
+async function changeLampButton(isLampOn, totalClicks) {
+  if (!isLampOn) {
+    lampButton.checked = false;
+
+  } else {
+    lampButton.checked = true;
+  }
+
+  clickCount.innerText = 'THIS LAMP HAS BEEN CHANGED ' + String(totalClicks) + ' TIMES';
+}
+
+async function changeLeaderboard(leaderboard) {
+  // TODO: !!
+}
+
+async function doLamp() {
+  // fetch request to change lamp
+  userClicks += 1; // local clicks
+  document.getElementById('userClicks').innerText = 'clicks: ' + String(userClicks);
+
+  fetch(`${server}/changeLamp`, {
+    method: 'POST',
+    body: JSON.stringify({'username': username}),
+    headers: {
+      'content-type': 'application/json'
     }
-  })
+  }).catch((error) => {
+      console.error('Error:', error);
+  });
 } 
+
+function createUser(ele) {
+  if (event.keyCode == 13) {
+    event.preventDefault();
+    username = ele.value
+
+    fetch(`${server}/createUser`, {
+      method: 'POST',
+      body: JSON.stringify({'username': username}),
+      headers: {
+        'content-type': 'application/json'
+      }
+    }).then(response => response.json())
+      .then(response => {
+      form.classList.add('animate__fadeOut')
+      localStorage.setItem('username', username);
+      getLamp() // sets userclicks and total clicks and lampbutton
+      bodySection.style.display = '';
+      introSection.classList.remove('blurry-text');
+      document.getElementById('username').innerText = username;
+      document.getElementById('userClicks').innerText = 'clicks: ' + String(userClicks);
+      userDiv.style.opacity = 1;
+
+      if (response.loggedIn) {
+        console.log('Welcome back! ' + username);
+      } else {
+        console.log('Welcome ' + username);
+      }
+
+    })
+      
+    
+  } 
 };
+
+function switchUser() {
+  localStorage.removeItem('username')
+  introSection.classList.add('blurry-text');
+  bodySection.style.display = 'none';
+  form.style.display = '';
+  form.classList.remove('animate__fadeOut')
+  form.classList.add('animate__fadeIn')
+  userDiv.style.opacity = 0;
+}
+
 lampButton.addEventListener('click', doLamp);
+
 form.addEventListener('mouseover', function() {
-window.setTimeout(function() {
-  usernameInput.focus();
-}, 0);
+  window.setTimeout(function() {
+    usernameInput.focus();
+  }, 0);
 });
